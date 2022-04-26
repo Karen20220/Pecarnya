d31e13ef22858af973ed37707d7f88fa5f234a1d
Карен Шахраманян <karenshahramanyan1@gmail.com>
2022-04-26 13:08:01 +0300

    gff

--- Pecarnya/MainWindow.xaml.cs
+++ Pecarnya/MainWindow.xaml.cs
@@ -24,5 +24,13 @@ namespace Pecarnya
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+
     }
 }
